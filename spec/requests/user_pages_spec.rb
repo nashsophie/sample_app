989e4b6b1569ce23defba24b7c1bd7a9f9b68adc
nash <p0pxyr@gmail.com>
2013-05-02 04:14:46 +0800

    http://about.ac/rails-tutorial-2nd-cn/chapter10.html 第10章

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,24 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+      before do
+        sign_in FactoryGirl.create(:user)
+        FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
+        FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+        visit users_path
+      end
+
+      it { should have_selector('title', text: 'All users') }
+      it { should have_selector('h1', text: 'All users') }
+      it "should list each user" do
+        User.all.each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+    
+  
   describe "signup page" do
     before { visit signup_path }
 
@@ -19,5 +37,21 @@ describe "User pages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+  
+  describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { visit edit_user_path(user) }
+
+      describe "page" do
+        it { should have_selector('h1', text: "Update your profile") }
+        it { should have_selector('title', text: "Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save changes" }
+        it { should have_content('error') }
+      end
+    end
   
 end
